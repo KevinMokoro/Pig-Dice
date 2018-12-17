74dffadf041f993ff3717af407b72f6c1624bc33
KevinMokoro <klosvoke1@gmail.com>
2018-12-17 20:16:28 +0300

    add all modifications incluiding identation.

--- README.md
+++ README.md
@@ -9,34 +9,39 @@ KEVIN MOKORO
  Its a website that allows two players to play dice on a concept of pig dice;player rolls and scores if turn score aint equal to 1 but if equal to one loses all the turn-total plus loses his/her turn to the opponent.It is based that a winner is the player who reaches 100 first.
 
 ### Technologies Used
+
 In developing this site the following were used:
+
 -   HTML
 -   CSS
 -   JAVASCRIPT
 -   jQuery
 -   Bootstrap
-## Setup link to page :
+    ## Setup link to page :
 
-Use the link below to _VISIT SITE_ : [github](https://kevinmokoro.github.io/Pig-Dice/. )
+Use the link below to _VISIT SITE_ : [github](https://kevinmokoro.github.io/Pig-Dice/.)
 
 ### usage
+
 Leisure.Simply its a game.
 
 #### BDD
-1. Get players' names.
-2. Assign each player scores.From that every time they roll the dice add their turn total to respective player scores.
-2. Lastly evaluate which player scores a value of more than 100 first and declare them the winner of that round of play.
+
+1.  Get players' names.
+2.  Assign each player scores.From that every time they roll the dice add their turn total to respective player scores.
+3.  Lastly evaluate which player scores a value of more than 100 first and declare them the winner of that round of play.
 
 ### support and contact details :
 
 klosvoke1@gmail.com or send message to 0701957703
 
 #### Contributing
+
 If you notice any changes that are necessary,discuss the changes with the email above.Any argument is welcome for discussion.
 
 #### Copyright and license
 
-Copyright <2018> <KEVIN MOKORO>
+Copyright &lt;2018> <KEVIN MOKORO>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,94 @@
-
-
 function Player(name, score) {
   this.name = name;
-  //this.turn = 0;
   this.score = 0;
 };
 
-function Play( player,turnTotal, roll) {
-this.player = player;
-this.turnTotal = 0;
-this.roll = 0;
+function Play(player, turnTotal, roll) {
+  this.player = player;
+  this.turnTotal = 0;
+  this.roll = 0;
 };
 Play.prototype.rollPlayer = function(player1, player2) {
-  var roll = Math.ceil(Math.random() * 6)
+  var roll = Math.ceil(Math.random() * 6);
   this.turnTotal += roll;
   if (roll !== 1) {
     this.roll += roll;
     return roll;
   } else {
     this.total = 0;
-    this.switchTurn(player1, player2)
+    this.switchTurn(player1, player2);
     return roll;
   };
 };
-Play.prototype.switchTurn = function(player1,player2) {
-  this.player = this.score+this.turnTotal;
+Play.prototype.switchTurn = function(player1, player2) {
+  this.player = this.score + this.turnTotal;
   this.turnTotal = 0;
   this.roll = 0;
   if (this.player === player1) {
     this.player = player2;
-    alert (player2);
+    alert(player2);
   } else if (this.player === player2) {
     this.player = player1;
-    alert (player1);
+    alert(player1);
   };
 };
 
 
 $(document).ready(function() {
 
-  //$(".btn").click(function(event) {
-    //$("#input").show();
-    //var player1Input = $("input#player1").val("");
-    //var player1 = new Player(player1Input);
-    //var player2Input = $("input#player2").val("");
-    //var player2 = new Player(player2Input);
-  //  $(".player1").text("player1Input");
-  //  $(".player2").text("player2Input");
-    //var first = new Play(player1)
-    //$("#names").hide();
-  //  event.preventDefault();
+  $(".btn").submit(function(event) {
+      $("#input").show();
+      var player1Input = $("input#player1").val("");
+      var player2Input = $("input#player2").val("");
+      $(".player1").text("player1Input");
+      $(".player2").text("player2Input");
 
- var player1 = new Player("Player 1");
- var player2 = new Player("Player 2");
- var current = new Play(player1);
+      $("#names").hide();
+      event.preventDefault();
+    };
+  };
 
-   $("#turn-total").text(total);
-   $('#outout1').text(player1.score);
-$('#output2').text(player2.score);
-$('#current_player').text(current.player.name);
+  var player1 = new Player("Player 1");
+  var player2 = new Player("Player 2");
+  var first = new Play(player1);
+  var current = new Play(player1);
 
-  $("form#roll").submit(function(event) {
+  $("#turn-total").text(total);
+  $("#output1").text(player1.score);
+  $("#output2").text(player2.score);
+  $("#current_player").text(current.player.name);
+
+  $("#roll").submit(function(event) {
     event.preventDefault();
     var result = current.rollPlayer(player1, player2);
-    $('#roll').text(result);
-    $('#turn-total').text(current.total);
+    $("#roll").text(result);
+    $("#turn-total").text(current.total);
 
     if ((current.total + current.player.score) >= 100) {
       if (current.player == player1) {
-          $('#outout1').text(current.total + current.player.score);
-          alert("You are the winner!");
+        $("#output1").text(current.total + current.player.score);
+        alert("player1 is the winner!");
       } else if (current.player == player2) {
-          $('#output2').text(current.total + current.player.score)
-          alert("You are the winner!");
-     }
-   };
+        $("#output2").text(current.total + current.player.score)
+        alert("Player 2 is the winner!");
+      }
+    };
   });
-    //$("btn#roll").click(function(event) {
+
+});
+$("#switch").submit(function(event) {
+event.preventDefault();
+
+current.switchTurn(player1, player2);
 
 
-      //event.preventDefault();
-  //  });
-  });
-  $("form#switch").submit(function(event) {
-  event.preventDefault();
-
-  current.switchTurn(player1, player2);
+$('#current_player').text(current.player.name);
 
 
-  $('#current_player').text(current.player.name);
+$("#output1").text(player1.score);
+$('#output2').text(player2.score);
 
-
-  $('#player1-score').text(player1.score);
-  $('#player2-score').text(player2.score);
-
-  //Prints the cleared Current Roll and Turn Total on page
-  $('#roll').text(current.randNumber);
-  $('#turn-total').text(current.total);
+$('#roll').text(current.roll);
+$('#turn-total').text(current.total);
 });
 })
